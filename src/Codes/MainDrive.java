12dc187b43ea0099eb53c131d705caf80cd100de
최현식 <chs1906@naver.com>
2021-07-05 21:04:43 +0900

    임시로 741를 문제로 선정

--- src/Codes/MainDrive.java
+++ src/Codes/MainDrive.java
@@ -7,6 +7,20 @@ public class MainDrive {
 //		숫자 야구 게임을 만듭니다.
 //		긴 풀이가 될 예정입니다. 단계별로 다시 복습해주세요.
 		
+//		임시 : 3자리 숫자를 => 3칸짜리 배열에 나눠서 저장. ex.741
+		
+		int[] questionNumbers = new int[3];
+		
+//		임시 : 직접 741 숫자를 저장.
+//		향후 : 컴퓨터가 랜덤 숫자 3개 뽑아서 저장.
+		
+		questionNumbers[0] = 7;
+		questionNumbers[1] = 4;
+		questionNumbers[2] = 1;
+		
+		
+		
+		
 		
 	}
 
