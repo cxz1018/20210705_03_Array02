8971427a5a8e43533316de239e3fe10ecaed42ef
최현식 <chs1906@naver.com>
2021-07-05 21:13:45 +0900

    3자리 숫자 계속 입력 + 각 자리별 배열에 배치

--- src/Codes/MainDrive.java
+++ src/Codes/MainDrive.java
@@ -1,5 +1,7 @@
 package Codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 	
 	public static void main(String[] args) {
@@ -18,7 +20,29 @@ public class MainDrive {
 		questionNumbers[1] = 4;
 		questionNumbers[2] = 1;
 		
+//		사용자에게 3자리 숫자를 무한히 입력받아보자.
+//		몇 번 입력해야 3S가 될까? => 반복 횟수가 불명확한 반복. => 무한 while문으로 활용.
 		
+		Scanner myScan = new Scanner(System.in);
+				
+		while (true) {
+			
+			System.out.print("3자리 숫자 입력 : ");
+			int inputNum = myScan.nextInt();
+			
+//			입력받은 숫자도 => 3자리 배열에 나눠서 저장.
+//			123 => [1,2,3]
+			
+			int[] inputNumbers = new int[3];
+			
+//			0번칸 : 123 => 1이 들어가야. 어떻게? 입력값을 100으로 나눈 값.			
+			inputNumbers[0] = inputNum / 100;
+//			1번칸 : 123 => 2가 들어가야. 어떻게? 123 => 12 => 1의자리
+			inputNumbers[1] = inputNum / 10 % 10;
+//			2번칸 : 123 => 3이 들어가야. 어떻게? 입력값을 10으로 나눈 나머지.
+			inputNumbers[2] = inputNum % 10;			
+			
+		}
 		
 		
 		
